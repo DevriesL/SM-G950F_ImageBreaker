f4be5511c48553642b1624d11bb670d85c4b979f
DevriesL <therkduan@gmail.com>
2017-07-05 22:55:06 +0800

    Add support for dream2lte

--- build.sh
+++ build.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+DEVICE=$1
+
 export ARCH=arm64
 
 export PATH=/home/devries/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin:$PATH
 
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-make exynos8895-dreamlte_eur_open_defconfig
+if [ "$DEVICE" = "plus" ]; then
+    make exynos8895-dream2lte_eur_open_defconfig
+else
+    make exynos8895-dreamlte_eur_open_defconfig
+fi
 
 make -j8
+
+rm arch/arm64/boot/dts/exynos/*.dtb
 
 make dtbs
 
 ./dtbTool -o dt.img -s 2048 -d arch/arm64/boot/dts/exynos/
 
-./mkbootfs ramdisk | lzma > ramdisk.packed
+if [ "$DEVICE" = "plus" ]; then
+    ./mkbootfs ramdisk_plus | lzma > ramdisk.packed
+else
+    ./mkbootfs ramdisk | lzma > ramdisk.packed
+fi
 
 ./mkbootimg \
       --kernel arch/arm64/boot/Image \
@@ -28,4 +40,9 @@ make dtbs
       --output boot.img
 
 echo SEANDROIDENFORCE >> boot.img
-tar -cvf boot.tar boot.img
+if [ "$DEVICE" = "plus" ]; then
+    mv boot.img dream2lte.img
+else
+    tar -cvf boot.tar boot.img
+    mv boot.img dreamlte.img
+fi
